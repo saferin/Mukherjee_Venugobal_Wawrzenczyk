2f53b4d22eee5b3434bf764b6938b4925305ba24
saferin <zyta.wawrzenczyk@gmail.com>
2015-09-20 22:34:04 +0200

    Update props.h
    Comments written by @Udhayan_Venugobal

--- props.h
+++ props.h
@@ -7,18 +7,18 @@
 
 #ifndef PROPS_H_
 #define PROPS_H_
-
+/* Structure to hold the values */
 struct property {
   char *key;
   char *value;
 };
-
+/* structure to define the collection  of the property*/
 struct properties {
   struct list *props;
 };
-
+/* object for the structure properties*/
 struct properties *parse_properties(char* chars);
-
+/* function declaration for - print properties */
 void print_properties(struct properties *props);
 
 #endif /* PROPS_H_ */
